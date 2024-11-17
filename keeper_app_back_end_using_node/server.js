f9d2edfc807d27ddcfbd59171e959db32675bed4
irshad <irshad.ali052@gmail.com>
2024-11-17 23:50:27 +0530

    some basic pakages are added like bcrypt and pg

--- keeper_app_back_end_using_node/server.js
+++ keeper_app_back_end_using_node/server.js
@@ -1,12 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pg from "pg";
-
+import pg, { Client } from "pg";
+const db=new pg.Client({
+    user:"postgres",
+    host:"localhost",
+    database:"keeper_db",
+    password:"root",
+    port:5432
+})
 const app=express();
 const port=3000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"))
-
+db.connect();
 
 
 app.listen(port,()=>{
